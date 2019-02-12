434c9110ff0b278dbe495f23bcd82e56a093af4b
Yassin Assim <yassinassim0@gmail.com>
2019-02-12 12:17:25 +0100

    create dev branch

--- src/components/front-office/welcome/Intro.js
+++ src/components/front-office/welcome/Intro.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
-import { Jumbotron, Button, Container } from 'react-bootstrap';
+import React, {Component} from 'react'
+import {Jumbotron, Button, Container} from 'react-bootstrap';
 // import { Button } from 'react-bootstrap';
 
 import './Intro.scss';
 
 class Intro extends Component {
     render() {
-      return (
-        <Jumbotron fluid 
-                   id="intro">
-            <Container>
-                <p>
-                    Achetez votre super carte aujourd'hui et bénéficiez de 30% de réduction !
-                </p>
-                <Button>
-                    Commander
-                </Button>
-            </Container>
-        </Jumbotron>    
-      );
+        return (
+            <Jumbotron fluid id="intro">
+                <Container>
+                    <p>
+                        Achetez votre super carte aujourd'hui et bénéficiez de 30% de réduction !
+                    </p>
+                    <Button>
+                        Commander
+                    </Button>
+                </Container>
+            </Jumbotron>
+        );
     }
-  }
-  
-  export default Intro;
+}
+
+export default Intro;
