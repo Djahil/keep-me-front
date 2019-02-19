374d89dbff8dceaeb2015b0f4c28514b4ea12c7e
Julien Laforge <julien.laforge@gmx.fr>
2019-02-19 17:29:28 +0100

    refacto VisitCard and api call handling in VisitCardPage

--- src/components/front-office/visit-card/VisitCardPage.js
+++ src/components/front-office/visit-card/VisitCardPage.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import VisitCard from './card/VisitCard';
 import './VisitCardPage.scss';
+import mockUser from '../../../assets/data/mockUser';
 
-const VisitCardPage = () => (
-    <div className="visit-card-page">
-        <VisitCard/>
-    </div>
-)
+class VisitCardPage extends Component {
+
+    state = {
+        cardInfos: {},
+        isMockMessageVisible: false,
+        isCardVisible: false
+    }
+
+    test = {
+        'logo': 'test',
+        'nom': 'test',
+        'prenom': 'test',
+        'poste': 'test',
+        'mail': 'test',
+        'entreprise': 'test',        
+    }
+
+    componentDidMount() {
+        const {match: {params} } = this.props
+        
+        fetch(`https://localhost:8001/api/${params.slug}/`)
+            .then(res => res.json())
+            .then(
+                res => {
+                    const isCardVisible = true
+                    this.setState({isCardVisible})
+                    const cardInfos = res;
+                    this.setState({cardInfos})
+                    console.log(res)
+                },
+                err => {
+                    const cardInfos = mockUser;
+                    this.setState({cardInfos})
+                    console.log(err)
+                    const isMockMessageVisible = true;
+                    this.setState({isMockMessageVisible})
+                }
+            )
+    }
+
+    render () {
+        return (
+            <div className="visit-card-page">
+                <VisitCard
+                    logo={this.state.cardInfos.logo}
+                    nom={this.state.cardInfos.nom}
+                    prenom={this.state.cardInfos.prenom}
+                    poste={this.state.cardInfos.poste}
+                    mail={this.state.cardInfos.mail}
+                    entreprise={this.state.cardInfos.entreprise}
+                />
+            </div>
+        )
+    }
+}
 
 export default VisitCardPage;
